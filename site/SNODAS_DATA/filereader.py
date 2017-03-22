f7ad3ce49ede283563a13d7ed4761f691c9c616d
Korysam15 <korysam@comcast.net>
2017-03-22 22:49:59 +0100

    Removed SNODAS_DATA file upon request of Steve Malers

--- site/SNODAS_DATA/filereader.py
+++
@@ -1,8 +0,0 @@
-import glob
-array = glob.glob("*.csv")
-array.sort(reverse=True)
-with open("ListOfDates.txt", 'w') as output_file:
-	for filename in array:
-		filename = filename.replace("SnowpackByDate_", "")
-		filename = filename.replace(".csv","")
-		output_file.write(filename + "\n")
